cd8454aeb936351a8a3857589fba989302917464
Rio Jos <riojosrj@gmail.com>
2024-10-01 13:50:18 +0530

    mongoose init

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:2963468692.
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://fresheat_mnklm:XmBYlDgkTbauGCet@cluster0.wwdhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+const User = mongoose.model('User', { name: String, email: String, phone: BigInt, password: String });
+
+
 
 
 //app config
@@ -14,6 +21,13 @@ app.use(cors())
 
 app.get("/" ,(req,res)=>{
     res.status(200).send("server started ")
+})
+
+app.post("/signup", (req, res) => {
+  const { name, email, phone, password } = req.body
+  const newUser = new User({ name, email, phone, password });
+
+  newUser.save().then(() => console.log('user created'));
 })
 
 app.listen(PORT,()=>console.log(`listening on localhost:${PORT}`))  
